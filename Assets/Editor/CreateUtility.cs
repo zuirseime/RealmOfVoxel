4c1582b55d4b95ad2b24020de5b78efc57350703
zuirseime <vovakibets88@gmail.com>
2025-01-31 15:13:27 +0200

    Added Room instantiation in the Editor

---
+++ Assets/Editor/CreateUtility.cs
@@ -0,0 +1,59 @@
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+public class CreateUtility : Editor
+{
+    [MenuItem("GameObject/Tools/Room")]
+    public static void CreateRoom(MenuCommand menuCommand)
+    {
+        GameObject room = new("Room");
+
+        CreateDoors(room);
+        CreateGeometry(room);
+        CreateBounds(room);
+
+        Place(room);
+    }
+
+    public static void Place(GameObject gObj)
+    {
+        gObj.transform.position = Vector3.zero;
+        GameObject.Instantiate
+        StageUtility.PlaceGameObjectInCurrentStage(gObj);
+        GameObjectUtility.EnsureUniqueNameForSibling(gObj);
+
+        Undo.RegisterCreatedObjectUndo(gObj, $"Create Object: {gObj.name}");
+        Selection.activeGameObject = gObj;
+
+        EditorSceneManager.MarkSceneDirty(EditorSceneManager.GetActiveScene());
+    }
+
+    private static void CreateDoors(GameObject room)
+    {
+        GameObject doors = new("Doors");
+        doors.transform.parent = room.transform;
+
+        GameObject door = new("Door (1)");
+        door.transform.position = Vector3.forward * 0.5f;
+        door.transform.parent = doors.transform;
+        IconManager.SetIcon(door, IconManager.IconColor.Red, IconManager.IconType.Circle);
+    }
+
+    private static void CreateGeometry(GameObject room)
+    {
+        GameObject geometry = new("Geometry");
+        geometry.transform.parent = room.transform;
+
+        GameObject box = ObjectFactory.CreatePrimitive(PrimitiveType.Cube);
+        box.transform.parent = geometry.transform;
+    }
+
+    private static void CreateBounds(GameObject room)
+    {
+        GameObject bounds = new("Bounds");
+        bounds.transform.parent = room.transform;
+        bounds.AddComponent<BoxCollider>();
+        IconManager.SetIcon(bounds, IconManager.IconColor.Green, IconManager.IconType.Diamond);
+    }
+}
---
+++ Assets/Editor/IconManager.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+public class IconManager
+{
+    public enum IconColor : byte
+    {
+        Gray, Blue, Teal, Green, Yellow, Orange, Red, Purple
+    }
+
+    public enum IconType : byte
+    {
+        Circle, Diamond
+    }
+
+    private static GUIContent[,] iconTextures;
+
+    public static void SetIcon(GameObject gObj, IconColor color, IconType type)
+    {
+        if (iconTextures is null)
+        {
+            iconTextures = LoadTextures();
+        }
+
+        Texture2D texture = GetIconTexture(color, type);
+        SetIcon(gObj, texture);
+    }
+
+    private static Texture2D GetIconTexture(IconColor color, IconType type)
+    {
+        return iconTextures[(int)color, (int)type].image as Texture2D;
+    }
+
+    private static GUIContent[,] LoadTextures()
+    {
+        byte colors = (byte)Enum.GetValues(typeof(IconColor)).Length;
+        byte types = (byte)Enum.GetValues(typeof(IconType)).Length;
+
+        GUIContent[,] textures = new GUIContent[colors, types];
+
+        for (int colorIndex = 0; colorIndex < colors; colorIndex++)
+        {
+            for (int typeIndex = 0; typeIndex < 2; typeIndex++)
+            {
+                int textureIndex = colorIndex + (typeIndex * colors);
+                textures[colorIndex, typeIndex] = LoadTexture("sv_icon_dot", textureIndex, "_pix16_gizmo");
+            }
+        }
+
+        return textures;
+    }
+
+    private static GUIContent LoadTexture(string baseName, int index, string postfix)
+    {
+        return EditorGUIUtility.IconContent(baseName + index + postfix);
+    }
+
+    private static void SetIcon(GameObject gObj, Texture2D texture)
+    {
+        EditorGUIUtility.SetIconForObject(gObj, texture );
+    }
+}
